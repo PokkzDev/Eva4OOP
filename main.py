1b3395d9f970fc287bc710e1ae1dff28e2cae8bf
matahari3403 <paolaarayamunoz90@gmail.com>
2024-12-10 23:12:24 -0300

    .

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from view.menu_principal import MenuPrincipal
 if __name__ == '__main__':
     menu = MenuPrincipal()
     menu.mostrar_menu()
+
+
+
+#admin: paola 
+#contraseña: 1234567
+
+#usuario: paolausuario
+#contraseña: 1234567
